16a9af89e06304569e8dd6cf745b86b120106eec
Leon Boschman <leon.boschman@gmail.com>
2022-10-23 11:50:52 +0200

    Disable all old stuff

--- src/App.svelte
+++ src/App.svelte
@@ -8,31 +8,23 @@
   <div class="navbar">
   <NavBar/>
   </div>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+
 
   <div class="card">
     <Counter />
   </div>
 
-  <p>
+  <!-- <p>
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
   </p>
 
   <p class="read-the-docs">
     Click on the Vite and Svelte logos to learn more
-  </p>
+  </p> -->
 </main>
 
 <style>
-  .logo {
+  /* .logo {
     height: 6em;
     padding: 1.5em;
     will-change: filter;
@@ -45,5 +37,5 @@
   }
   .read-the-docs {
     color: #888;
-  }
+  } */
 </style>
