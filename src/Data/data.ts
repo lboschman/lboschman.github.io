3820273a5bd361b2bef012fa8b73a3dcdc423d77
Leon Boschman <leon.boschman@gmail.com>
2022-11-01 15:00:20 +0100

    Data to put in the pages

---
+++ src/Data/data.ts
@@ -0,0 +1,41 @@
+const HEADER = "Leon Boschman";
+
+const NAVBAR_DATA = [
+  { id: 1, url: "/", label: "Home" },
+  { id: 2, url: "#about", label: "About me" },
+  { id: 3, url: "#projects", label: "My projects" },
+];
+
+const MYSELF_DATA = {
+    HEADING: "About me",
+    TITLE: "What I am good at",
+    GOOD_AT_LIST: [
+        "Data analysis",
+        "Communicating abstract concepts clearly",
+        "Writing clean, concise code",
+    ],
+};
+
+const ABOUT_DATA = {
+  HEADING: "Why choose me?",
+  TITLE: "Why I am the right choice",
+  WHY_CHOOSE_US_LIST: [
+    "We provides Cost-Effective Digital Marketing than Others.",
+    "High customer statisfaction and experience.",
+    "Marketing efficiency and quick time to value.",
+    "Clear & transparent fee structure.",
+    "We provides Marketing automation which is an integral platform that ties all of your digital marketing together.",
+    "A strong desire to establish long lasting business partnerships.",
+    "Provide digital marketing to mobile consumer.",
+    "We provides wide range to services in reasonable prices",
+  ],
+};
+
+const DATA = {
+    HEADER,
+    NAVBAR_DATA,
+    ABOUT_DATA,
+    MYSELF_DATA,
+}
+
+export default DATA;
